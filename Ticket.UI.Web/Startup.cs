723dbed858d435ba9c363f4e3f6dee2195c19e33
Max Kimambo <max@kimambo.de>
2015-12-13 15:31:01 +0100

    Initial project structure

---
+++ Ticket.UI.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ticket.UI.Web.Startup))]
+namespace Ticket.UI.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
