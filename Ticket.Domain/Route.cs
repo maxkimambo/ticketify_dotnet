563140776a2c44f8eedc6904094b9c413b7e88ee
ayubu <ayubumasasi@gmail.com>
2015-12-15 21:46:32 +0300

    Added the domain object for RouteStops (In a give route there are points where passengers will drop or enter in the car)

--- Ticket.Domain/Route.cs
+++ Ticket.Domain/Route.cs
@@ -5,5 +5,8 @@
         public string Id { get; set; }
         public Location Start { get; set; }
         public Location Destination { get; set; }
+
+
+        public System.Collections.Generic.List<RouteStop> RouteStops { get; set; }
     }
 }
